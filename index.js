039a14fcfb807d6465e5555b0665bb46f4b570cd
aitrics-ray <kyounghwan.choi@aitrics.com>
2024-01-04 23:14:10 +0900

    fix

--- index.js
+++ index.js
@@ -39,43 +39,28 @@ async function handleApprove() {
 
 }
 
-async function handleComment(octokit) {
-  const { payload } = Github.context;
-  const commentUrl = payload.comment ? payload.comment.html_url : null;
-  const prOwner = payload.issue ? payload.issue.user.login : null;
-  const prUrl = payload.issue ? payload.issue.html_url : null;
-  const commanter = payload.comment ? payload.comment.user.login : null;
-  const commentBody = payload.comment ? payload.comment.body : null;
-  const prTitle = payload.issue ? payload.issue.title : null;
-
-  slackUserMapping(prOwner, commanter);
-  if (commentBody && commanter && commentUrl) {
-    sendSlackMessage(commentBody, commanter, commentUrl, prOwner, prTitle, prUrl);
-  }
-}
-
-async function getUserNameToGitHub(octokit, githubName) {
-  try {
-    const res = await octokit.request('GET /users/{username}', {
-      username: githubName,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-    });
-    return res.data.name; // GitHub 사용자의 실제 이름 반환
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    return null; // 에러 발생 시 null 반환
-  }
-}
-
-async function slackUserMapping(octokit, prOwner, commanter) {
+async function slackUserMapping(octokit, web, prOwner, commanter) {
   try {
     const ownerName = await getUserNameToGitHub(octokit, prOwner);
     const commanterName = await getUserNameToGitHub(octokit, commanter);
 
-    console.log('PR Owner Name:', ownerName);
-    console.log('Commenter Name:', commanterName);
+    const ownerSlackId = await findSlackUserIdByName(web, ownerName);
+    const commanterSlackId = await findSlackUserIdByName(web, commanterName);
+    return { ownerSlackId, commanterSlackId };
+  } catch (error) {
+    console.error('Error in slackUserMapping:', error);
+  }
+}
+
+
+async function slackUserMapping(octokit, web, prOwner, commanter) {
+  try {
+    const ownerName = await getUserNameToGitHub(octokit, prOwner);
+    const commanterName = await getUserNameToGitHub(octokit, commanter);
+
+    const ownerSlackId = await findSlackUserIdByName(web, ownerName);
+    const commanterSlackId = await findSlackUserIdByName(web, commanterName);
+    return { ownerSlackId, commanterSlackId };
   } catch (error) {
     console.error('Error in slackUserMapping:', error);
   }
@@ -97,6 +82,49 @@ function sendSlackMessage(commentBody, commanter, commentUrl, prOwner, prTitle, 
   };
 
   web.chat.postMessage(message);
+}
+
+
+async function handleComment(octokit, web) {
+  const { payload } = Github.context;
+  const commentUrl = payload.comment ? payload.comment.html_url : null;
+  const prOwner = payload.issue ? payload.issue.user.login : null;
+  const prUrl = payload.issue ? payload.issue.html_url : null;
+  const commanter = payload.comment ? payload.comment.user.login : null;
+  const commentBody = payload.comment ? payload.comment.body : null;
+  const prTitle = payload.issue ? payload.issue.title : null;
+
+  const { ownerSlackId, commanterSlackId } = await slackUserMapping(octokit, web, prOwner, commanter);
+  if (commentBody && commanter && commentUrl) {
+    sendSlackMessage(commentBody, commanterSlackId, commentUrl, ownerSlackId, prTitle, prUrl);
+  }
+}
+
+async function findSlackUserIdByName(web, searchName) {
+  try {
+    const result = await web.users.list();
+    const user = result.members.find((member) => (
+      member.real_name && member.real_name.includes(searchName))
+      || (member.profile.display_name && member.profile.display_name.includes(searchName)));
+    return user ? user.id : null;
+  } catch (error) {
+    console.error('Slack API error:', error);
+  }
+}
+
+async function getUserNameToGitHub(octokit, githubName) {
+  try {
+    const res = await octokit.request('GET /users/{username}', {
+      username: githubName,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    });
+    return res.data.name; // GitHub 사용자의 실제 이름 반환
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    return null; // 에러 발생 시 null 반환
+  }
 }
 
 run();
