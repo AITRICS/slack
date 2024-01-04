8d249c3d372fbe4aafeb497795e38186a23c7ad6
aitrics-ray <kyounghwan.choi@aitrics.com>
2024-01-04 21:39:39 +0900

    wip

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
 const { WebClient } = require('@slack/web-api');
-const { Octokit } = require("@octokit/core");
-const Core = require("@actions/core");
+const { Octokit } = require('@octokit/core');
+const Core = require('@actions/core');
 const Github = require('@actions/github');
 
-const SLACK_TOKEN = Core.getInput("SLACK_TOKEN");
-const GITHUB_TOKEN = Core.getInput("GITHUB_TOKEN");
-const ACTION_TYPE = Core.getInput("ACTION_TYPE");
-const OWNER_REPO = Core.getInput("OWNER_REPO");
+const SLACK_TOKEN = Core.getInput('SLACK_TOKEN');
+const GITHUB_TOKEN = Core.getInput('GITHUB_TOKEN');
+const ACTION_TYPE = Core.getInput('ACTION_TYPE');
+const OWNER_REPO = Core.getInput('OWNER_REPO');
 
 const SLACK_FRONTEND_CHANNEL_ID = 'C06B5J3KD8F';
-const SLACK_BACKEND_CHANNEL_ID =  'C06C8TLTURE';
+const SLACK_BACKEND_CHANNEL_ID = 'C06C8TLTURE';
 const SLACK_SE_CHANNEL_ID = 'C06CS5Q4L8G';
 
 async function run() {
-    const web = new WebClient(SLACK_TOKEN);
-    const octokit = new Octokit({ auth: GITHUB_TOKEN });
+  const web = new WebClient(SLACK_TOKEN);
+  const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
+  try {
     switch (ACTION_TYPE) {
-        case 'approve':
-            await handleApprove();
-            break;
-        case 'comment':
-            await handleComment(octokit);
-            break;
-        default:
-            console.log('error');
-            break;
+      case 'approve':
+        await handleApprove();
+        break;
+      case 'comment':
+        await handleComment(octokit, web);
+        break;
+      default:
+        console.log('error');
+        break;
     }
-    const payload = Github.context.payload;
-    const commentUrl = payload.comment ? payload.comment.html_url : null;
-    const commentId = payload.comment ? payload.comment.id : null;
-    const prOwner = payload.pull_request ? payload.pull_request.user.login : null;
+  } catch (error) {
+    console.error('Error executing action:', error);
+  }
 
-    console.log("Comment ID:", commentId);
-    console.log(payload);
-    console.log('Done!!');
+  console.log('Done!!');
 }
 
 async function handleApprove() {
@@ -42,23 +40,30 @@ async function handleApprove() {
 }
 
 async function handleComment(octokit) {
-    const payload = Github.context.payload;
-    const commentUrl = payload.comment ? payload.comment.html_url : null;
-    const commentId = payload.comment ? payload.comment.id : null;
-    const prOwner = payload.pull_request ? payload.pull_request.user.login : null;
-    const commanter = payload.comment ? payload.comment.user.login : null;
-    const commentBody = payload.comment ? payload.comment.body : null;
+  const { payload } = Github.context;
+  const commentUrl = payload.comment ? payload.comment.html_url : null;
+  const commentId = payload.comment ? payload.comment.id : null;
+  const prOwner = payload.pull_request ? payload.pull_request.user.login : null;
+  const commanter = payload.comment ? payload.comment.user.login : null;
+  const commentBody = payload.comment ? payload.comment.body : null;
 
-    sendSlackMessage(commentBody, commanter, commentUrl, prOwner);
+  if (commentBody && commanter && commentUrl) {
+    sendSlackMessage(commentBody, commanter, commentUrl);
+  }
 }
 
 function sendSlackMessage(commentBody, commanter, commentUrl, prOwner) {
-    const web = new WebClient(SLACK_TOKEN);
+  const web = new WebClient(SLACK_TOKEN);
 
-    web.chat.postMessage({
-        channel: SLACK_FRONTEND_CHANNEL_ID,
-        text: `*${commanter}* commented on PR <${commentUrl}|${commentBody}>`
-    });
+  const message = `*${commanter}* commented on PR of *${prOwner}*:\n>`
+      + `${commentBody.substring(0, 100)}...`
+      + `\nSee more <${commentUrl}|here>.`;
+
+  web.chat.postMessage({
+    channel: SLACK_FRONTEND_CHANNEL_ID,
+    text: message,
+    mrkdwn: true, // 활성화하여 Markdown을 사용할 수 있습니다.
+  });
 }
 
 run();
