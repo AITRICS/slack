72360e44a379e6afe456539df647e7eb4a0355af
aitrics-ray <kyounghwan.choi@aitrics.com>
2024-01-08 21:33:12 +0900

    fix

--- slack/slackMessages.js
+++ slack/slackMessages.js
@@ -50,7 +50,7 @@ class SlackMessages {
       text: `\`\`\`${commentData.commentDiff}\`\`\`\n*코맨트 내용 :*\n${commentData.commentBody}\n\n<${commentData.commentUrl}|코멘트 보러가기>\n\n`,
     }];
 
-    const message = this.#createMessage(
+    const message = SlackMessages.#createMessage(
       channelId,
       `*<${commentData.prUrl}|${commentData.prTitle}>*\n:pencil: *${commentData.commentAuthorSlackRealName}* 님이 코멘트를 남겼어요!! <@${commentData.mentionedSlackId}>:\n`,
       attachments,
@@ -65,7 +65,7 @@ class SlackMessages {
       text: `${commentData.commentBody}\n\n<${commentData.commentUrl}|코멘트 보러가기>.`,
     }];
 
-    const message = this.#createMessage(
+    const message = SlackMessages.#createMessage(
       channelId,
       `*<${commentData.prUrl}|${commentData.prTitle}>*\n:white_check_mark: *${commentData.reviewerSlackRealName}* 님이 Approve를 했습니다!! <@${commentData.ownerSlackId}>:\n`,
       attachments,
@@ -80,7 +80,7 @@ class SlackMessages {
       text: `\n<${commentData.prUrl}|PR 보러가기>.`,
     }];
 
-    const message = this.#createMessage(
+    const message = SlackMessages.#createMessage(
       channelId,
       `*<${commentData.prUrl}|${commentData.prTitle}>*\n:eyes: *${commentData.ownerSlackRealName}* 님이 Review를 요청했습니다!! <@${commentData.reviewerSlackId}>:\n`,
       attachments,
