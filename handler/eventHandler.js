b53632920779ae028f4f60d75a8ae9fd3652f272
aitrics-ray <kyounghwan.choi@aitrics.com>
2024-01-08 17:08:57 +0900

    fix

--- handler/eventHandler.js
+++ handler/eventHandler.js
@@ -98,9 +98,8 @@ class EventHandler {
     commentData.ownerSlackId = await this.#getSlackUserProperty(commentData.prOwnerGitName, 'id');
     commentData.commenterSlackRealName = await this.#getSlackUserProperty(commentData.commenterGitName, 'realName');
     console.log(commentData);
-    if (commentData.commentBody && commentData.commenterGitName && commentData.commentUrl) {
-      await this.slackMessages.sendSlackMessageToReviewRequested(commentData, channelId);
-    }
+
+    await this.slackMessages.sendSlackMessageToReviewRequested(commentData, channelId);
   }
 }
 
