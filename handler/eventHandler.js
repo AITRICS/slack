a0777423d547687966034aa395cfe9851867de31
aitrics-ray <kyounghwan.choi@aitrics.com>
2024-01-09 11:06:08 +0900

    Modify prUrl in bandleComment

--- handler/eventHandler.js
+++ handler/eventHandler.js
@@ -96,7 +96,7 @@ class EventHandler {
     const commentData = {
       commentUrl: payload.comment?.html_url,
       mentionedGitName: payload.issue?.user.login ?? payload.pull_request?.user.login,
-      prUrl: payload.issue?.html_url ?? payload.pull_request?.html_url,
+      prUrl: payload.pull_request?.html_url,
       commentAuthorGitName: payload.comment?.user.login,
       commentBody: payload.comment?.body,
       prTitle: payload.issue?.title ?? payload.pull_request?.title,
